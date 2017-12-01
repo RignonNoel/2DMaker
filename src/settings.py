f2153417aad3d1902c878604c08d76b7b88f2983
RignonNoel <rignon.noel@gmail.com>
2017-11-30 21:17:59 -0500

    create new settings file

---
+++ src/settings.py
@@ -0,0 +1,9 @@
+import os
+
+PROJECT_NAME = "My first game"
+FPS = 60
+RESOLUTION = 45, 30
+TILES_WIDTH = 16
+
+TILES_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tiles/')
+MAPS_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'maps/')
